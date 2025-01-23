0bc2a89fd53c72f1c81228ebab579f7921a784ab
Pintahito <pintahito14@gmail.com>
2025-01-23 08:13:10 -0600

    modificacion

--- src/components/compEmpleado/EmpleadoObraList.js
+++ src/components/compEmpleado/EmpleadoObraList.js
@@ -47,7 +47,6 @@ function EmpleadoList({ empleadosO, setEmpleadoEditadoO, setModalType, setEmplea
             cell: (row) => (
                 <div className="flex space-x-2">
                     <button
-                    disabled
                         className="bg-blue-500 text-white py-1 px-3 rounded-md hover:bg-blue-600 transition"
                         onClick={() => {
                             setEmpleadoEditadoO(row);
